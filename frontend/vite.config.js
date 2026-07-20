edda016827adc6ee35f71a55a2fa78206100f58b
sohani84 <sohani4@gmail.com>
2026-07-20 17:56:42 +0530

    frontend files updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server: {
     host: "0.0.0.0",
-    port: 5173,
-    strictPort: true,
+    port: 5173
   },
-
   preview: {
     host: "0.0.0.0",
-    port: 4173,
+    port: 4173
   },
-
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-  },
+    sourcemap: false
+  }
 });
